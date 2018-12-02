2d4774f4f3ff9a90736cd36d357edbc4b51b6e4a
Diego Cardenas <cardenasrdiego@gmail.com>
2018-12-02 15:43:14 +0100

    Seeding db with some orders bikes and customer

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+josh = Customer.find_or_create_by(name: 'Joshua Burke', email: 'joshuaburke@email.com')
+kevin = Customer.find_or_create_by(name: 'Kevin Heart', email: 'kevinheart@email.com')
+classic_bike = Bike.find_or_create_by(name: 'Classic Bike', price: 300)
+cheap_bike = Bike.find_or_create_by(name: 'Cheap Bike', price: 100)
+joshs_order = order.find_or_create_by(customer_id: josh.id, total: 400, bikes: [cheap_bike, classic_bike])
+kevin_order = order.find_or_create_by(
+  customer_id: kevin.id,
+  total: 500, 
+  bikes: [cheap_bike, cheap_bike, cheap_bike, cheap_bike, cheap_bike]
+)
