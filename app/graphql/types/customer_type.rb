644db84ad8c3e204bb1ebc39735480c8a160353a
Diego Cardenas <cardenasrdiego@gmail.com>
2018-12-02 12:28:53 +0100

    Installed gql gem Added Customer to the db model and gql type

---
+++ app/graphql/types/customer_type.rb
@@ -0,0 +1,9 @@
+Types::CustomerType = GraphQL::ObjectType.define do
+  # this type is named `Customer`
+  name 'Customer'
+
+  # it has the following fields
+  field :id, !types.ID
+  field :name, !types.String
+  field :email, !types.String
+end
